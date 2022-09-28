f7834cdd692d1627d4f37cd4c65cde30d9ba42db
Shekharkhot <shekarkhot2@gmail.com>
2022-09-29 00:32:17 +0530

    Initial commit

---
+++ conditional statements/If else.py
@@ -0,0 +1,108 @@
+# # waptcw the given int num is even or odd
+# if it is even then square of the number
+# if it is odd then print cube of the number
+# num = int(input("enter the number-"))
+# if num % 2 == 0:
+#     print(num**2)
+# else:
+#     print(num**3)
+
+
+# #waptcw the given data is single value data or collection data
+# data = 2.5
+# if type(data) in [int,float,complex,bool]:
+#     print("Its a single value data")
+# else:
+#     print("Its a collection data")
+
+
+# #waptcw the given data is mutable or immutable
+# data_ = {"a":1}  #(1,2.5,"data",{1,2,3},[1,"sas",],1+9j)
+# if type(data_) in (list,set,dict):
+#     print("its mutable data")
+# else:
+#     print("its immutable data")
+
+
+# # waptcw the given data is consonant or not
+# consonant = input('ENTER THE DATA-')
+# if consonant in "AEIOUaeiou":
+#     print("its consonant")
+# else:
+#     print("its not consonant")
+
+
+# waptcw the char is spl char or not
+# special = input("enter the character-")
+# if not  ('a' <= special <= 'z' or 'A' <= special <= 'Z' or '0' <= special <= '9'):
+#     print("its a spl character")
+# else:
+#     print("not spl character")
+
+
+# #waptcw the given string palindrome or not
+# word = str(input("enter the word-"))
+# if word[0] == word[-1]:
+#     print("its a palindrome")
+# else:
+#     print("not a palindrome")
+
+
+# # waptcw the given number is having 4 digits or not if it is having 4 digits
+# then print number as it is if it is having other than 4 digits then print by reversing the numbers
+# num = str(input("enter the number-"))
+# if len(str(num)) == 4:
+#     print(num)
+# else:
+#     print(num[::-1])
+
+
+# # There are monkeys sitting in the class
+# a parameter is A smile is 1, if the monkey A is smiling,the parameter B smile is 1 when monkey B is smiling
+# we are in trouble if the both monkey are smiling or none of the monkey are smiling if we are in trouble print happy else print sad
+# a_smile = int(input("A smile"))
+# b_smile = int(input("B smile"))
+# if a_smile == 0 and b_smile == 0:
+#     print("Sad")
+# else:
+#     print("Happy")
+
+
+# # wap to print all the values present that odd index in a given list
+# only if it is having 5 values in it else print length of the given list
+# values = list(input("enter values-"))
+# if len(values) == 5:
+#     print(values[1::2])
+# else:
+#     print(len(values))
+
+
+# # waptcw the given string is palindrome or not
+# word = str(input("enter the word-"))
+# if word[0] == word[-1]:
+#     print("its a palindrome")
+# else:
+#     print(" not a palindrome ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
